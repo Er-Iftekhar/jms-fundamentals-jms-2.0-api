fe6e4f29e5aae3bbd4af34be721e3b84c0daab70
Syed Iftekharuddin <syeydy636@gmail.com>
2025-05-08 11:30:35 +0200

    Add initial demo project with JMS 2

---
+++ jms-2-jms-api-general-usage/src/main/java/com/app/jms_2_jms_api_general_usage/Jms2JmsApiGeneralUsageApplication.java
@@ -0,0 +1,13 @@
+package com.app.jms_2_jms_api_general_usage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jms2JmsApiGeneralUsageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jms2JmsApiGeneralUsageApplication.class, args);
+	}
+
+}
---
+++ jms-2-jms-api-general-usage/src/test/java/com/app/jms_2_jms_api_general_usage/Jms2JmsApiGeneralUsageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.jms_2_jms_api_general_usage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jms2JmsApiGeneralUsageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
